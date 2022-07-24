891dcc51742d3c383f99f22da85710a83eb53d22
vishal <neurake7@gmail.com>
2022-07-24 22:49:39 +0530

    link update

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -1,5 +1,5 @@
 <?php 
-	define('SITEURL', 'https://wetlet/phpblog/');
+	define('SITEURL', 'https://wetlet.com/phpblog/');
 	define('LOCALHOST', 'localhost');
 	define('USERNAME', 'u271036748_phpblog');
 	define('PASSWORD', 'Vishal@5676');
